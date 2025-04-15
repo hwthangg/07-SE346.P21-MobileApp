f2ab7842e51b0e9b8297fe5f26ac7a452fd4ec54
hwthangg <hwthang.2510@gmail.com>
2025-04-15 09:09:02 +0700

    add notification screen, nav admin & leader

--- frontend/app/(app)/admin/_layout.tsx
+++ frontend/app/(app)/admin/_layout.tsx
@@ -1,12 +1,108 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { Stack, Tabs } from 'expo-router'
+import { router, Stack, Tabs } from 'expo-router'
+import { Ionicons, Octicons } from '@expo/vector-icons'
 
 const AdminLayout = () => {
   return (
-    <Tabs>
-        <Tabs.Screen name='accounts/index'/>
-        <Tabs.Screen name='chapters/index'/>
+    <Tabs  screenOptions={{
+      tabBarStyle: {
+        height: 70,
+      },
+    }}>
+        <Tabs.Screen name='accounts/index' options={{
+          headerShown: false,
+          tabBarButton: ({
+            accessibilityState,
+          }: {
+            accessibilityState: any;
+          }) => {
+            const focused = accessibilityState?.selected;
+
+            return (
+              <TouchableOpacity
+                onPress={() => router.push("/(app)/admin/accounts")}
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: focused ? "#e0f0ff" : "white",
+                }}
+              >
+                <Octicons
+                  name="people"
+                  size={30}
+                  color={focused ? "blue" : "gray"}
+                />
+                <Text style={{ color: focused ? "blue" : "gray" }}>
+                  Tài khoản
+                </Text>
+              </TouchableOpacity>
+            );
+          },
+        }}/>
+        <Tabs.Screen name='chapters/index' options={{
+          headerShown: false,
+          tabBarButton: ({
+            accessibilityState,
+          }: {
+            accessibilityState: any;
+          }) => {
+            const focused = accessibilityState?.selected;
+
+            return (
+              <TouchableOpacity
+                onPress={() => router.push("/(app)/admin/chapters")}
+                style={{
+                  flex: 1,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: focused ? "#e0f0ff" : "white",
+                }}
+              >
+                <Octicons
+                  name="organization"
+                  size={30}
+                  color={focused ? "blue" : "gray"}
+                />
+                <Text style={{ color: focused ? "blue" : "gray" }}>
+                Chi đoàn
+                </Text>
+              </TouchableOpacity>
+            );
+          },
+        }}/>
+
+         <Tabs.Screen
+                name="settings/index"
+                options={{
+                  headerShown: false,
+                  tabBarButton: ({
+                    accessibilityState,
+                  }: {
+                    accessibilityState: any;
+                  }) => {
+                    const focused = accessibilityState?.selected;
+        
+                    return (
+                      <TouchableOpacity
+                        onPress={() => router.push("/(app)/admin/settings")}
+                        style={{
+                          flex: 1,
+                          alignItems: "center",
+                          justifyContent: "center",
+                          backgroundColor: focused ? "#e0f0ff" : "white",
+                        }}
+                      >
+                        <Ionicons name="settings-outline" size={30}  color={focused ? "blue" : "gray"}/>
+                        <Text style={{ color: focused ? "blue" : "gray" }}>
+                          Cài đặt
+                        </Text>
+                      </TouchableOpacity>
+                    );
+                  },
+                }}
+              />
         
     </Tabs>
   )
--- frontend/app/(app)/leader/notifications/index.tsx
+++ frontend/app/(app)/leader/notifications/index.tsx
@@ -1,12 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import React from "react";
 
 const Notification = () => {
-  return (
-    <View>
-      <Text>Notification</Text>
-    </View>
-  )
-}
+  const data = [
+    {
+      id: 1,
+      text: "Sự kiện “Chương trình họp mặt mừng kỉ niệm 30/4” sẽ bắt đầu sau 24 giờ nữa Sự kiện “Chương trình họp mặt mừng kỉ niệm 30/4” sẽ bắt đầu sau 24 giờ nữa",
+    },
+    {
+      id: 2,
+      text: "Sự kiện “Chương trình họp mặt mừng kỉ niệm 30/4” sẽ bắt đầu sau 24 giờ nữa",
+    },
+    {
+      id: 3,
+      text: "Sự kiện “Chương trình họp mặt mừng kỉ niệm 30/4” sẽ bắt đầu sau 24 giờ nữa",
+    },
+  ];
 
-export default Notification
+  const renderItem = ({item} : {item:any}) =>{
+    return(
+<TouchableOpacity
+          style={{
+           minHeight:60,
+            borderWidth: 1,
+            boxShadow: "0px 5px 9px #4955CE",
+            opacity: 0.5,
+            borderRadius: 10,
+            padding: 10,
+            marginVertical: 10,
+            overflowY: "visible"
+          }}
+        >
+          <Text style={{ flex: 1, borderWidth: 1, verticalAlign: "middle" }}>
+            {item.text}
+          </Text>
+        </TouchableOpacity>
+    )
+  }
+  
+  return (
+    <View className="flex-1">
+      <View className="h-[90px] bg-[#3E4FF5] rounded-bl-[30px] rounded-br-[30px]">
+        <Text
+          className="flex-1 text-white text-center text-[20px] font-bold m-auto"
+          style={{ verticalAlign: "middle" }}
+        >
+          Thông báo
+        </Text>
+      </View>
+      <View className="flex-1 h-[60px] w-[87.5%] border m-auto">
+        <FlatList
+        data={data}
+        keyExtractor={(item:any)=>item.id.toString()}
+        renderItem={renderItem}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Notification;
